8d1249a3afcbe2b124a84f7e09d502b15db55357
Jody7 <jody7777PVP@gmail.com>
2016-09-14 21:22:16 -0700

    creating drawing classes

---
+++ src/MainDraw.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainDraw extends JPanel{
+
+    public void resetBG(){
+        this.setBackground(Color.WHITE);
+    }
+
+    public void mainTask(Graphics g){
+
+    }
+
+    public void draw(Graphics g){
+        super.paintComponent(g);
+    }
+
+}
---
+++ src/MainThread.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainThread {
+    static Graphics graphics;
+    public static JFrame CreateFrame(){
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,300);
+        frame.setVisible(true);
+        graphics = frame.getGraphics();
+        return frame;
+    }
+
+    public static void main(String[] args){
+        JFrame frame = CreateFrame();
+        MainDraw main = new MainDraw();
+        main.resetBG();
+        frame.add(main);
+
+        main.mainTask(graphics);
+    }
+}
