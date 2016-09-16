91d67356d2c06af1525713e0a154f7d2622414f0
Jody7 <jody7777PVP@gmail.com>
2016-09-15 18:34:32 -0700

    writing rotation transforms

--- src/maths/RotMatrix.java
+++ src/maths/RotMatrix.java
@@ -2,6 +2,9 @@ package maths;
 
 public class RotMatrix {
     class RotationMatrix{
+
+        //http://www.c-jump.com/bcc/common/Talk3/Math/Matrices/Matrices.html
+
         public double matrix4[];
         /*
             double matrix4[] = {
@@ -11,33 +14,48 @@ public class RotMatrix {
             d, h, l, 1
             };
          */
-        int a, b, c, d, e, f, g, h, i, j, k, l;
-        public RotationMatrix(int aa,int bb,int cc,int dd,int ee,int ff,int gg,int hh, int ii, int jj, int kk, int ll){
+        int a, b, c, d, e, f, g, h, i;
+        public RotationMatrix(int aa,int bb,int cc,int dd,int ee,int ff,int gg,int hh, int ii){
             a = aa;
             b = bb;
             c = cc;
+
             d = dd;
             e = ee;
             f = ff;
+
             g = gg;
             h = hh;
             i = ii;
-            j = jj;
-            k = kk;
-            l = ll;
 
             double matrix4[] = {
-                    a, e, i, 0,
-                    b, f, j, 0,
-                    c, g, k, 0,
-                    d, h, l, 1
+                    a, b, c, 0,
+                    d, e, f, 0,
+                    g, h, i, 0,
+                    0, 0, 0, 1 // i think the padded 0's are for quaternions, which i can implement later
             };
-
         }
     }
 
 
-    public void rX(){
+    public void rX(RotationMatrix rotM){
+        RotationMatrix rotNew = new RotationMatrix(0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+        double a,b,c,d,e,f,g,h,i;
+        double j,k,l,m,n,o,p,q,r;
+        /*
+        j   k   l
+        m   n   o
+        p   q   r
+         */
+
+        a = rotM.a; b = rotM.b; c = rotM.c;
+        d = rotM.d; e = rotM.e; f = rotM.f;
+        g = rotM.g; h = rotM.h; i = rotM.i;
+
+        j = 1;
+
+
     }
     public void rY(){
 
